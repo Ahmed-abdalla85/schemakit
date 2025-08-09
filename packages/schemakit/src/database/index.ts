42e7323b231852d39949646bf669f0b12b8b5545
Ahmed-abdalla85 <ahmedabdalla_85@hotmail.com>
2025-08-09 15:48:10 +0300

    Clean unused files

--- packages/schemakit/src/database/index.ts
+++ packages/schemakit/src/database/index.ts
@@ -12,7 +12,6 @@ export { DatabaseAdapter } from './adapter';
 
 // Specific adapters
 export { DrizzleAdapter } from './adapters/drizzle';
-export { DrizzleAdapter_old } from './adapters/drizzle_old';
 export { InMemoryAdapter } from './adapters/inmemory';
 
 // Database management utilities
